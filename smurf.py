45bf30eccd4292852dd07a05c0887c1d992d3267
Sufyan <somalik@ualberta.ca>
2021-01-30 18:54:35 -0700

    Completed file for categorizing data

--- smurf.py
+++ smurf.py
@@ -1,40 +1,43 @@
 import csv
 
 class DataPoint:
-    def __init__(self, time, heelVoltage, ballVoltage, ankleVelocity):
-        self.time = time
+    def __init__(self, heelVoltage, ballVoltage, bigtoeVoltage, littletoeVoltage):
         self.heelVoltage = heelVoltage
         self.ballVoltage = ballVoltage
-        self.ankleVelocity = ankleVelocity
+        self.bigtoeVoltage = bigtoeVoltage
+        self.littletoeVoltage = littletoeVoltage
 
 class DataFile:
     def __init__(self, filename):
         self.inputFile = filename
         self.midheelVoltage = 0
         self.midballVoltage = 0
+        self.midbigVoltage = 0
+        self.midlittleVoltage = 0
         self.dataPoints = []
         self.sortedData = {}
     
     def collectData(self):
         i = 0
-        startTime = 0
         with open(self.inputFile, encoding='utf-8-sig') as inputData:
             reader = csv.reader(inputData)
             for line in reader:
-                time = float(line[0][-6:])
-                if i == 0:
-                    startTime += time
-                heelVoltage = float(line[1])
-                ballVoltage = float(line[2])
-                ankleVelocity = float(line[3])
+                heelVoltage = float(line[0])
+                ballVoltage = float(line[1])
+                bigtoeVoltage = float(line[2])
+                littletoeVoltage = float(line[3])
                 self.dataPoints.append(
-                    DataPoint(time - startTime, heelVoltage, ballVoltage, ankleVelocity)
+                    DataPoint(heelVoltage, ballVoltage, bigtoeVoltage, littletoeVoltage)
                     )
                 self.midheelVoltage += heelVoltage
                 self.midballVoltage += ballVoltage
+                self.midbigVoltage += bigtoeVoltage
+                self.midlittleVoltage += littletoeVoltage
                 i += 1
             self.midheelVoltage /= i
             self.midballVoltage /= i
+            self.midbigVoltage /= i
+            self.midlittleVoltage /= i
     
     def sortData(self):
         initialContact = []
@@ -43,27 +46,29 @@ class DataFile:
         terminalStance = []
         preSwing = []
         swingPhase = []
+        errors = 0
         for data in range(len(self.dataPoints)):
-            if self.dataPoints[data].heelVoltage >= self.midheelVoltage and self.dataPoints[data].ballVoltage < self.midballVoltage and abs(self.dataPoints[data].ankleVelocity) > 1:
+            if self.dataPoints[data].heelVoltage >= self.midheelVoltage and self.dataPoints[data].ballVoltage < self.midballVoltage and self.dataPoints[data].bigtoeVoltage < self.midbigVoltage:
                 initialContact.append(self.dataPoints[data])
             
-            elif self.dataPoints[data].heelVoltage >= self.midheelVoltage and self.dataPoints[data].ballVoltage < self.midballVoltage and abs(self.dataPoints[data].ankleVelocity) < 1:
+            elif self.dataPoints[data].heelVoltage >= self.midheelVoltage and self.dataPoints[data].ballVoltage >= self.midballVoltage and self.dataPoints[data].bigtoeVoltage < self.midbigVoltage:
                 loadingResponse.append(self.dataPoints[data])
             
-            elif self.dataPoints[data].heelVoltage >= self.midheelVoltage and self.dataPoints[data].ballVoltage >= self.midballVoltage and abs(self.dataPoints[data].ankleVelocity) < 1:
+            elif self.dataPoints[data].heelVoltage >= self.midheelVoltage and self.dataPoints[data].ballVoltage >= self.midballVoltage and self.dataPoints[data].bigtoeVoltage >= self.midbigVoltage:
                 midStance.append(self.dataPoints[data])
             
-            elif self.dataPoints[data].heelVoltage < self.midheelVoltage and self.dataPoints[data].ballVoltage >= self.midballVoltage and abs(self.dataPoints[data].ankleVelocity) < 1:
+            elif self.dataPoints[data].heelVoltage < self.midheelVoltage and self.dataPoints[data].ballVoltage >= self.midballVoltage and self.dataPoints[data].bigtoeVoltage >= self.midbigVoltage:
                 terminalStance.append(self.dataPoints[data])
             
-            elif self.dataPoints[data].heelVoltage < self.midheelVoltage and self.dataPoints[data].ballVoltage >= self.midballVoltage and abs(self.dataPoints[data].ankleVelocity) > 1:
+            elif self.dataPoints[data].heelVoltage < self.midheelVoltage and self.dataPoints[data].ballVoltage < self.midballVoltage and self.dataPoints[data].bigtoeVoltage >= self.midbigVoltage:
                 preSwing.append(self.dataPoints[data])
             
-            elif self.dataPoints[data].heelVoltage < self.midheelVoltage and self.dataPoints[data].ballVoltage < self.midballVoltage:
+            elif self.dataPoints[data].heelVoltage < self.midheelVoltage and self.dataPoints[data].ballVoltage < self.midballVoltage and self.dataPoints[data].bigtoeVoltage < self.midbigVoltage:
                 swingPhase.append(self.dataPoints[data])
             
             else:
                 errors += 1
+
         self.sortedData['initialContact'] = initialContact
         self.sortedData['loadingResponse'] = loadingResponse
         self.sortedData['midStance'] = midStance
@@ -74,7 +79,7 @@ class DataFile:
     def exportData(self):
         return self.sortedData
 
-smurf = DataFile("heelballdata.csv")
+smurf = DataFile("data.csv")
 smurf.collectData()
 smurf.sortData()
 smurf.exportData()
